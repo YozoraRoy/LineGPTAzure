3cdf05c7c53d283cfbd0e8401da7750d89e01604
Roy <yuzora_roy@hotmail.com>
2023-05-29 17:56:46 +0900

    fix the keyvalue.cs as a example 2

--- LineGPTAzureFunctions/Helper/KeyValueSetting.cs
+++ LineGPTAzureFunctions/Helper/KeyValueSetting.cs
@@ -12,7 +12,7 @@ namespace LineGPTAzureFunctions.Helper
 
 
         // Line[Notifacation]
-        public string lineNotifacationToken { get; } = "mzqlxldyXfzL7PLQQZXhUJqKpsZIZVCfnsWONMcbp2o";
+        public string lineNotifacationToken { get; } = "";
 
         public string lineMessagingApiUrl { get; } = "https://api.line.me/v2/bot/message/reply";
         public string lineMessagingApiContentUrl { get; } = "https://api-data.line.me/v2/bot/message/";
@@ -20,23 +20,24 @@ namespace LineGPTAzureFunctions.Helper
         public string lineRequestUrl { get; } = "https://api.line.me/v2/bot/profile/";
         public string lineNotifyUrl { get; } = "https://notify-api.line.me/api/notify";
 
-        public string lineChannelAccessToken { get; } = @"jbwg5RbX/5A47Gg/xGgvMVE0WMFNjlpYzDrc2fyAGO07qikKownm3Wu4u7mrTbu15VgoqZgq/RfGo2RM0WlgHGpw/gSSa/BWNyGYx8tJaNioffVXTiGUBnjbsSNzijJEkAy9GA3w9XQKZCiCb7SLxwdB04t89/1O/w1cDnyilFU=";
-        public string linechannelSecret { get; } = "1045973fc88d32d25dd2eb22586ddbed";
-
+        // -- Line Roybot
+        public string lineChannelAccessToken { get; } = @"";
+        public string linechannelSecret { get; } = "";
+          
         //Azure CosmosDB
-        public string databaseId { get;  } = "ChatRecorder";
-        public string containerId { get; } = "Conversion";
-        public string ChatGPTAPIKey { get; } = "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";
+        public string databaseId { get;  } = "databaseId";
+        public string containerId { get; } = "containerId";
+        public string ChatGPTAPIKey { get; } = "sk-xxxx";
         public string CosmosEndpointUri { get; } = "https://cosmoschatdb.documents.azure.com:443/";
-        public string CosmosPrimaryKey { get; } = "7LMUXtMhRPPPOETwvw2ODimhCD93TpxsLTt4Z37YdiZTXrVjcC5YsTfKnCiMXo2TbUArK2HC9Vl4ACDbwuMFEg==";
+        public string CosmosPrimaryKey { get; } = "==";
 
         //Azure Speech
-        public string speechKey { get; } = "c28b66aaf54c4ca283de7fc4d0dd92de";
-        public string speechRegion { get; } = "japaneast";
+        public string speechKey { get; } = "";
+        public string speechRegion { get; } = "";
 
 
         // chartGPT
         public string systemSetup { get; } =
-            "你是一個日本語教師和同時也是中文老師，住在日本，之前也住過台灣，如果問你問題，會用英文和日文進行回答，不知道的問題就會說不知道，不會隨便猜或者組合答案進行回覆。而且你知道跟你聊天的人是:";
+            " You are a Japanese teacher and you know who is talking  to you:";
     }
 }
